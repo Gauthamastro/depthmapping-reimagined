abcd6e22d82e227a8eb63f7def702d82ca80fdc0
Akash Raut <36098149+codeakash@users.noreply.github.com>
2018-10-27 21:01:35 +0530

    Add files via upload
    incomplete network

---
+++ y_net.py
@@ -0,0 +1,28 @@
+import conv_block
+import decoder
+
+
+
+
+img_width,img_height = 1024,1024
+
+img_input = Input(shape=(img_width, img_height, 3))
+
+conv_model = Model(input = img_input, output = conv_block(img_input))
+
+conv_model.summary()
+
+
+auto_enc_dec_model = Model(input = img_input, output = decoder_block(img_input))
+
+auto_enc_dec_model.summary()
+
+
+
+#now rectify the syntax and various other errors
+#figure out a way to get the encoder output from the auto_enc_dec_model so as to act as label for the conv_model
+# then compile the models with suitable loss and regularisation
+#and then all we need to preprocess the images and disparity images
+#do some intial training of the auto_enc_dec_model 
+# then iteratively train the conv_model taking encoder output as label, then the auto_enc_dec_model to give better encoding
+# can also exchange weights if stable training is needed
