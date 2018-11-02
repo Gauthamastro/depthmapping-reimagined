61a700e53f9472de9b38fb82ab0f933250302286
Akash Raut <36098149+codeakash@users.noreply.github.com>
2018-11-03 03:31:34 +0530

    Update y_net_tensorflow.py
    just a barebone encoder decoder without skip connections and untested.
    need to work on skip connections and other options like batch norm dropout in tensorflow and need to make the network big

--- y_net_tensorflow.py
+++ y_net_tensorflow.py
@@ -1,7 +1,8 @@
 import tensorflow as tf 
 import numpy as np
+import cv2
 
-
+(img_height,img_width,channels)=(480, 640, 3)
 # use xavier initialliser and conv2d_transpose
 observationsx=tf.placeholder(shape=[None,img_width,img_height,channels],dtype=tf.float32,name='observations')
 
@@ -9,8 +10,8 @@ initializer=tf.contrib.layers.xavier_initializer()
 ##nn for the enc dec
 
 #encoder
-
-ew1= tf.Variable(tf.initializer([5,5,channels,64]))
+seed=32
+ew1= tf.Variable(initializer([5,5,channels,64]))#[h,w,inputchannels,outputchannels]
 ew2= tf.Variable(tf.random_normal([5,5,64,64], seed=seed))
 ew3= tf.Variable(tf.random_normal([img_height*img_width*64,1024], seed=seed))
 ew4= tf.Variable(tf.random_normal([1024,512], seed=seed))
@@ -45,40 +46,53 @@ el4 = tf.nn.relu(el4)
 elout = tf.add(tf.matmul(el4, ew5),eb5)
 elout = tf.nn.relu(elout)
 
-encoder_output=tf.multinomial(logits=lout,num_samples=1)
+#encoder_output=tf.multinomial(logits=lout,num_samples=1)
 
 #decoder with conv2dtrans without skip connections
-"""
-w1= tf.Variable(tf.initializer([5,5,channels,64]))
-w2= tf.Variable(tf.random_normal([5,5,64,64], seed=seed))
-w3= tf.Variable(tf.random_normal([img_height*img_width*64,1024], seed=seed))
-w4= tf.Variable(tf.random_normal([1024,512], seed=seed))
-w5= tf.Variable(tf.random_normal([512,216], seed=seed))
+
+dw1= tf.Variable(initializer([5,5,1,64]))#[h,w,opchannels,inchannels]
+dw2= tf.Variable(tf.random_normal([5,5,64,64], seed=seed))
+dw3= tf.Variable(tf.random_normal([1024,img_width*img_height*64], seed=seed))
+dw4= tf.Variable(tf.random_normal([512,1024], seed=seed))
+dw5= tf.Variable(tf.random_normal([216,512], seed=seed))
 
 
-b1= tf.Variable(tf.random_normal([64], seed=seed))
-b2= tf.Variable(tf.random_normal([64], seed=seed))
-b3= tf.Variable(tf.random_normal([1024], seed=seed))
-b4= tf.Variable(tf.random_normal([512], seed=seed))
-b5= tf.Variable(tf.random_normal([216], seed=seed))
+db1= tf.Variable(tf.random_normal([1], seed=seed))
+db2= tf.Variable(tf.random_normal([64], seed=seed))
+db3= tf.Variable(tf.random_normal([img_width*img_height*64], seed=seed))
+db4= tf.Variable(tf.random_normal([1024], seed=seed))
+db5= tf.Variable(tf.random_normal([512], seed=seed))
 
-dl1=tf.add(tf.matmul(elout,dw1),db1)
+dl1=tf.add(tf.matmul(elout,dw5),db5)
 dl1=tf.nn.relu(dl1)
-"""
+
+dl2=tf.add(tf.matmul(dl1,dw4),db4)
+dl2=tf.nn.relu(dl2)
+
+
+dl3=tf.add(tf.matmul(dl2,dw3),db3)
+dl3=tf.nn.relu(dl3)
+
+un_flat=tf.reshape(dl3,[-1,img_height,img_width,64])
+
+dl4=tf.add(tf.nn.conv2d_transpose(un_flat,dw2,output_shape=[-1,img_height,img_width,64],strides=[1,1,1,1],padding='SAME'),db2)
+dl4=tf.nn.relu(dl4)
+
+dl5=tf.add(tf.nn.conv2d_transpose(dl4,dw1,output_shape=[-1,img_height,img_width,1],strides=[1,1,1,1],padding='SAME'),db1)
+
+output_image=tf.nn.relu(dl5)#find a better activation here
+
+#loss=
+
+
 
 with tf.Session() as sess:
 	tf.initialize_all_variables().run()
-	(img_height,img_width,channels)=np.shape(input_img)
-	#input_img =input_img.ravel()
-	#inpt=inpt.reshape(img_height,img_width,channels)
-	"""
-	sess.run(valid_pad)
-
-	sess.run(same_pad)
-	print(valid_pad.get_shape() )
-	print(same_pad.get_shape())
-	a,b,c,d=valid_pad.get_shape()
-	print a+b+c+d
-	"""
-	action=sess.run(sample_op,feed_dict={observationsx:[input_img]})
+	input_img=cv2.imread(path="/home/akash/Desktop/my_car/real_data/f(0).jpg")
 	
+	print np.shape(input_img)
+	
+	output=sess.run(output_image,feed_dict={observationsx:[input_img]})
+
+input_img=cv2.imread("/home/akash/Desktop/my_car/real_data/f(0).jpg")
+print np.shape(input_img)#(480, 640, 3)
