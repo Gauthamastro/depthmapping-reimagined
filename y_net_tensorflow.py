0b931599efd37c8e767cb05b7a7d23795c071e29
Akash Raut <36098149+codeakash@users.noreply.github.com>
2018-11-03 22:35:27 +0530

    skip connections and making everything into a class left.

--- y_net_tensorflow.py
+++ y_net_tensorflow.py
@@ -1,10 +1,15 @@
 import tensorflow as tf 
 import numpy as np
 import cv2
-
+import time
+from matplotlib import pyplot as plt
+from PIL import Image
 (img_height,img_width,channels)=(100,100, 3)
 # use xavier initialliser and conv2d_transpose
-observationsx=tf.placeholder(shape=[None,img_width,img_height,channels],dtype=tf.float32,name='observations')
+#batch_size=tf.placeholder(shape=[None],dtype=tf.uint8,name='batch_size')
+batch_size=1
+learning_rate=tf.placeholder(shape=[None],dtype=tf.float32,name='learning_rate')
+observationsx=tf.placeholder(shape=[batch_size,img_width,img_height,channels],dtype=tf.float32,name='observations')
 
 initializer=tf.contrib.layers.xavier_initializer()
 ##nn for the enc dec
@@ -28,14 +33,14 @@ eb5= tf.Variable(tf.random_normal([216], seed=seed))
 
 
 
-el1 = tf.add(tf.nn.conv2d(observationsx,ew1,strides=[1,1,1,1],padding='SAME'),eb1)
+el1 = tf.add(tf.nn.conv2d(observationsx,ew1,strides=[batch_size,1,1,1],padding='SAME'),eb1)
 el1 = tf.nn.relu(el1)
 
-el2 = tf.add(tf.nn.conv2d(el1, ew2,strides=[1,1,1,1],padding='SAME'),eb2)
+el2 = tf.add(tf.nn.conv2d(el1, ew2,strides=[batch_size,1,1,1],padding='SAME'),eb2)
 el2 = tf.nn.relu(el2)
 
 
-flat= tf.reshape(el2,[-1,img_height*img_width*64])
+flat= tf.reshape(el2,[batch_size,img_height*img_width*64])
 el3 = tf.add(tf.matmul(flat, ew3),eb3)
 el3 = tf.nn.relu(el3)
 
@@ -75,10 +80,10 @@ dl3=tf.nn.relu(dl3)
 
 un_flat=tf.reshape(dl3,[-1,img_height,img_width,64])
 
-dl4=tf.add(tf.nn.conv2d_transpose(un_flat,dw2,output_shape=[-1,img_height,img_width,64],strides=[1,1,1,1],padding='SAME'),db2)
+dl4=tf.add(tf.nn.conv2d_transpose(un_flat,dw2,output_shape=[batch_size,img_height,img_width,64],strides=[batch_size,1,1,1],padding='SAME'),db2)
 dl4=tf.nn.relu(dl4)
 
-dl5=tf.add(tf.nn.conv2d_transpose(dl4,dw1,output_shape=[-1,img_height,img_width,1],strides=[1,1,1,1],padding='SAME'),db1)
+dl5=tf.add(tf.nn.conv2d_transpose(dl4,dw1,output_shape=[batch_size,img_height,img_width,1],strides=[batch_size,1,1,1],padding='SAME'),db1)
 
 output_image=tf.nn.relu(dl5)   #find a better activation here
 
@@ -90,10 +95,17 @@ with tf.Session() as sess:
 	tf.initialize_all_variables().run()
 	input_img=cv2.imread("/home/akash/Desktop/my_car/real_data/f(0).jpg")
 	input_img= cv2.resize(input_img,(100,100))
+	#cv2.imshow('input image',input_img)
 	print np.shape(input_img)
-	output=sess.run(dl4,feed_dict={observationsx:[input_img]})
-	print output
-	print np.shape(output)
+	i = Image.fromarray(input_img)
+	i.show()
+	output=sess.run(output_image,feed_dict={observationsx:[input_img]})
+	#print output
+	print np.shape(output[0])
+	output=output.ravel()
+	output=np.reshape(output,(100,100))
+	i = Image.fromarray(output)
+	i.show()
 	#output=sess.run(output_image,feed_dict={observationsx:[input_img]})
 """
 input_img=cv2.imread("/home/akash/Desktop/my_car/real_data/f(0).jpg")
