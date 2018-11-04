f697bebe257c28175f6ae294f04ba950b1956922
Gautham J <Gauthamastro@gmail.com>
2018-11-04 09:57:14 +0530

    code for layers and parameters done
    now going to code the decoding of these to real model in keras and then logic for crossover and mutation

--- GA/genetic.py
+++ GA/genetic.py
@@ -9,13 +9,6 @@ random.seed(34)
 
 
 class conv_layers:
-	def __init__(self):
-		self.output_filter_depth_list = [2,4,16,32,64,128,256,512,1024] # assumeing channels last
-		self.kernel_size_list = [1,3,5,7,9,11]
-		self.strides_list = [1,2,3,4,5]
-		self.padding_list = ['same','valid']
-		self.data_format = ['channels_last','channels_first']
-
 
 	def C2D(self,filters,kernel_size,strides,padding,data_format,activation):
 		self.layer = Conv2D(filters,kernel_size, strides=strides,
@@ -29,12 +22,6 @@ class conv_layers:
 
 
 class convT_layers:
-	def __init__(self):
-		self.output_filter_depth_list = [2,4,16,32,64,128,256,512,1024] # assumeing channels last
-		self.kernel_size_list = [1,3,5,7,9,11]
-		self.strides_list = [1,2,3,4,5]
-		self.padding_list = ['same','valid']
-		self.data_format = ['channels_last','channels_first']
 
 	def C2DT(self,filters,kernel_size,strides,padding,data_format,activation):
 		self.layer=keras.layers.Conv2DTranspose(filters, kernel_size, strides=strides, 
@@ -46,9 +33,6 @@ class convT_layers:
 												 padding=padding, output_padding=None, data_format=data_format,activation=activation)
 		return self.layer
 class drop_layers :
-	def __init__(self):
-		self.data_format_list = ['channels_last','channels_first']
-		self.droputout_rate_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,]
 
 	def DO(self,rate):
 		self.layer = Dropout(rate,seed =seed)
@@ -59,8 +43,6 @@ class drop_layers :
 		return self.layer
 
 class dense_layers:
-	def __init__(self):
-		self.units = [2,4,16,32,64,128,256,512,1024,2048]
 
 	def D(self,units,activation):
 		self.layer = Dense(units, activation=activation)
@@ -72,11 +54,8 @@ class merge_layers:
 		return self.layer
 
 class activations:
-	def __init__(self):
-		self.alpha_list = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-
 	def LeakyReLU_act(self,layer,alpha=0.3):
-		self.layer = LeakyReLU(alpha=0.3)(layer)
+		self.layer = LeakyReLU(alpha=alpha)(layer)
 		return self.layer
 
 	def PReLU_act(self,layer):
@@ -84,23 +63,18 @@ class activations:
 		return self.layer
 
 	def ELU_act(self,layer,alpha=1.0):
-		self.layer = ELU()(layer)
+		self.layer = ELU(alpha)(layer)
 		return self.layer
 
 	def ThresoldedReLU_act(self,layer,theta=1.0):
 		self.layer = ThresoldedReLU(theta)(layer)
 		return self.layer
 
-	def relu_act(self,layer,max_value=None, negative_slope=0.0, threshold=0.0):
+	def relu_act(self,layer, negative_slope=0.0, threshold=0.0):
 		self.layer = ReLU(max_value,negative_slope,threshold)(layer)
 		return self.layer
 
 class pooling_layers:
-	def __init__(self):
-		self.pool_size_list = [1,2,3,4,5]
-		self.strides_list = [1,2,3,4,5]
-		self.padding_list = ['same','valid']
-		self.data_format_list = ['channels_last','channels_first']
 
 	def MaxPooling2D_layer(self,input_layer,pool_size=(2, 2), strides=None, padding='valid', data_format=None):
 		self.layer = MaxPooling2D(pool_size=pool_size, strides=strides, padding=padding, data_format=data_format)(input_layer)
@@ -119,9 +93,6 @@ class pooling_layers:
 		return self.layer
 
 class normalization:
-	def __init__(self):
-		self.momentum_list = [0.9,0.99,0.999]
-		self.epsilon_list = [0.001,0.01,0.1]
 
 	def BatchNormalization_layer(axis,momentum,epsilon):
 		self.layer = keras.layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001) #axis is done by assuming channels last
@@ -130,6 +101,7 @@ class normalization:
 
 class GA:
 	def __init__(self,max_conv_layers,max_convT_layers,max_skip_connections,input_shape):
+		#Layers available for genetic alogorithm to make
 		self.special_conv_layers = ['Conv2D','SeparableConv2D']
 		self.special_convT_layers =['Conv2DTranspose','UpSampling2D']
 		self.drop_layers = ['Dropout','SpatialDropout2D']#No 1
@@ -144,7 +116,23 @@ class GA:
 		self.max_skip_connections=max_skip_connections
 		self.input_shape = input_shape	
 
+		#Params that genetic algorithm will use to search!
+		self.output_filter_depth_list = [2,4,16,32,64,128,256,512,1024] # assumeing channels last
+		self.kernel_size_list = [1,3,5,7,9,11]
+		self.strides_list = [1,2,3,4,5]
+		self.padding_list = ['same'] #['same','valid']
+		self.data_format_list = ['channels_last'] #['channels_last','channels_first']
+		self.momentum_list = [0.9,0.99,0.999]
+		self.epsilon_list = [0.001,0.01,0.1]
+		self.pool_size_list = [1,2,3]
+		self.units = [2,4,16,32,64,128,256,512,1024,2048]
+		self.alpha_list = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+		self.droputout_rate_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 
+
+
+	def pickone(self,list): #helper function
+		return list[random.randint(0,(len(list)-1))]
 
 
 	def init_genome(self):
@@ -188,42 +176,96 @@ class GA:
 			for j in range(len(self.genome[i])):
 				if self.genome[i][j] == 'special_conv_layers':
 					if random.randint(0,1):
-						self.genome[i][j] ='Conv2D'
+						self.gene = {'layer':'Conv2D',
+										'filters':self.pickone(self.output_filter_depth_list),
+										'kernel_size':self.pickone(self.kernel_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										'activation':self.pickone(self.activations)}
+						self.genome[i][j] = self.gene
 					
 					else:
-						self.genome[i][j] = 'SeparableConv2D'
+						self.gene = {'layer':'SeparableConv2D',
+										'filters':self.pickone(self.output_filter_depth_list),
+										'kernel_size':self.pickone(self.kernel_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										'activation':self.pickone(self.activations)}
+						self.genome[i][j] = self.gene
 					
 				if self.genome[i][j] == 'special_convT_layers':
 					if random.randint(0,1):
-						self.genome[i][j] ='Conv2DTranspose'
+						self.gene = {'layer':'Conv2DTranspose',
+										'filters':self.pickone(self.output_filter_depth_list),
+										'kernel_size':self.pickone(self.kernel_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										'activation':self.pickone(self.activations)}
+						self.genome[i][j] = self.gene
 					
 					else:
-						self.genome[i][j] = 'UpSampling2D'
+						self.gene = {'layer':'UpSampling2D',
+										'filters':self.pickone(self.output_filter_depth_list),
+										'kernel_size':self.pickone(self.kernel_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										'activation':self.pickone(self.activations)}
+						self.genome[i][j] = self.gene
 					
 				if self.genome[i][j] == 'pooling_layers':
 					k  = random.randint(1,4)
 					if k == 1:
-						self.genome[i][j] ='MaxPooling2D'
+						self.gene = {'layer':'MaxPooling2D',
+										'pool_size':self.pickone(self.pool_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										}
+						self.genome[i][j] = self.gene
 					
 					if k == 2:
-						self.genome[i][j] = 'GlobalMaxPooling2D'
+						self.gene = {'layer':'GlobalMaxPooling2D',
+										'pool_size':self.pickone(self.pool_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										}
+						self.genome[i][j] = self.gene
 					
 					if k == 3:
-						self.genome[i][j] = 'AveragePooling2D'
+						self.gene = {'layer':'AveragePooling2D',
+										'pool_size':self.pickone(self.pool_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										}
+						self.genome[i][j] = self.gene
 					
 					if k == 4:
-						self.genome[i][j] = 'GlobalAveragePooling2D'
+						self.gene = {'layer':'GlobalAveragePooling2D',
+										'pool_size':self.pickone(self.pool_size_list),
+										'strides':self.pickone(self.strides_list),
+										'padding':self.pickone(self.padding_list),
+										'data_format':self.pickone(self.data_format_list),
+										}
+						self.genome[i][j] = self.gene
 					
 				if self.genome[i][j] == 'drop_layers':
 					if random.randint(0,1):
-						self.genome[i][j] = 'Dropout'
+						self.gene = {'layer':'Dropout',
+										'rate':self.pickone(self.droputout_rate_list),
+										'data_format':self.pickone(self.data_format_list)}
+						self.genome[i][j] = self.gene
 					
 					else:
-						self.genome[i][j] = 'SpatialDropout2D'
-					
-
-		for i in self.genome:#For debugging
-			print(i)  #for debugging
+						self.gene = {'layer':'SpatialDropout2D',
+										'rate':self.pickone(self.droputout_rate_list),
+										'data_format':self.pickone(self.data_format_list)}
+						self.genome[i][j] = self.gene
 
 		return self.genome 
 
@@ -233,11 +275,12 @@ class GA:
 
 	def decode_genome(self,genome):
 		#genome is a list of layers and their params
-		pass
+		for i in self.genome:# for debugging only!
+			for j in i:
+				print(j)
 
 
 
 
-
-a = GA(15,10,0,(100,100,3),)
-a.init_genome()
+a = GA(20,15,0,(100,100,3),)
+a.decode_genome(a.init_genome())
